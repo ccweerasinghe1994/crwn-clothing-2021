d4434993ce09adb4222d75f96448a932bd1f78f9
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-07-02 10:38:59 +0530

    added the folder structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.scss";
+import HomePage from "./pages/homepage/home-page.component";
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
