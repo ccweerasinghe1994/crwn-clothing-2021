f8dd8823008e9b7a4fa3bbd662ad5b2e905821d6
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-07-03 07:37:04 +0530

    adding react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import logo from "./logo.svg";
 import "./App.scss";
 import HomePage from "./pages/homepage/home-page.component";
+import { Route, Switch } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
