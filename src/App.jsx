169d93d3eec96a1aff38487fadb98f53c9bd7932
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-07-06 14:41:12 +0530

    added the signin component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import "./App.scss";
 import HomePage from "./pages/homepage/home-page.component";
 import ShopPage from "./pages/shop/shop-page.component";
-import { Route, Switch } from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
-  return (
-    <div>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-      </Switch>
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Switch>
+                <Route exact path="/" component={HomePage}/>
+                <Route path="/shop" component={ShopPage}/>
+                <Route path="/signin" component={SignInAndSignUpPage}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import './sign-in.styles.scss';
+import FormInput from "../forminput/form-input.component";
+import CustomButton from "../custom-button/custom-buttom.component";
+
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+            email: "",
+            password: ""
+        })
+    }
+    handleChane = (event) => {
+        const {name, value} = event.target;
+        this.setState({[name]: value})
+    }
+
+    render() {
+        return (
+            <div className={'sign-in'}>
+                <h2>i already have an account</h2>
+                <span>sign in with your email and password</span>
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput label="email" handleChange={this.handleChane} type="email" name={"email"}
+                               value={this.state.email} required/>
+                    <FormInput label="password" handleChange={this.handleChane} type="password" name={"password"}
+                               value={this.state.password}
+                               required/>
+
+                    <CustomButton type="submit">Sign In</CustomButton>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn;
