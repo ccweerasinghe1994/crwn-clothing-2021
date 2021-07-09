aae747e4af01813f1c2859ca07cb403928902be2
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-07-09 10:02:37 +0530

    added the sign up component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import ShopPage from "./pages/shop/shop-page.component";
 import {Route, Switch} from "react-router-dom";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import {auth} from './firebase/firebase.utils'
+import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 
 class App extends React.Component {
     constructor(props) {
@@ -19,16 +19,31 @@ class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({
-                currentUser: user
-            })
-            console.log(user)
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+
+            if (userAuth) {
+                const useRef = await createUserProfileDocument(userAuth);
+                useRef.onSnapshot(snapshot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapshot.id,
+                            ...snapshot.data()
+                        }
+                    })
+                })
+
+            } else {
+                this.setState({
+                    currentUser: userAuth
+                })
+            }
+
         })
     }
 
     componentWillUnmount() {
-        this.unsubscribeFromAuth = null
+        this.unsubscribeFromAuth();
+
     }
 
     render() {
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -22,6 +22,30 @@ provider.setCustomParameters({
     prompt: 'select_account'
 })
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+
+    if (!userAuth) return;
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapshot = await userRef.get();
+    if (!snapshot.exists) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (e) {
+            console.log("error creating a new user", e.message)
+        }
+
+    }
+    return userRef;
+}
+
 export const signInWithGoogle = () => auth.signInWithPopup(provider)
 
 export default firebase;
