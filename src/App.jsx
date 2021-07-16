da394f86a8c8ead8c05f9deb352f6978957f2cb2
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-07-16 16:12:22 +0530

    initial redux implementation is complete

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,29 @@ import {Route, Switch} from "react-router-dom";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import {auth, createUserProfileDocument} from './firebase/firebase.utils'
+import {connect} from 'react-redux';
+import {setCurrentUser} from "./redux/user/user.actions";
 
 class App extends React.Component {
-    constructor(props) {
 
-        super(props);
-        this.state = {
-            currentUser: null
-        }
-    }
 
     unsubscribeFromAuth = null;
 
     componentDidMount() {
+        const {setCurrentUser} = this.props;
         this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
 
             if (userAuth) {
                 const useRef = await createUserProfileDocument(userAuth);
                 useRef.onSnapshot(snapshot => {
-                    this.setState({
-                        currentUser: {
-                            id: snapshot.id,
-                            ...snapshot.data()
-                        }
+                    setCurrentUser({
+                        id: snapshot.id,
+                        ...snapshot.data()
                     })
                 })
 
             } else {
-                this.setState({
-                    currentUser: userAuth
-                })
+                setCurrentUser(userAuth)
             }
 
         })
@@ -61,4 +54,7 @@ class App extends React.Component {
     }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+    setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+export default connect(null, mapDispatchToProps)(App);
