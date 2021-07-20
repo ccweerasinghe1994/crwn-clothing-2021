cda2dc441753572d72685270f48ba5d82ec6cec4
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-07-20 06:22:46 +0530

    checkout component is finished

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import {cartActionTypes} from "./cart-action.types";
-import {addItemToCart} from "./cart.utils";
+import {addItemToCart, removeCartItem} from "./cart.utils";
 
 const INITAIL_STATE = {
     hidden: true,
@@ -18,6 +18,16 @@ const cartReducer = (state = INITAIL_STATE, action) => {
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            }
+        case cartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
+            }
+        case cartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeCartItem(state.cartItems, action.payload)
             }
         default:
             return state;
