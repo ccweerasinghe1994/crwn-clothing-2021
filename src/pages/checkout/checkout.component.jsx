e7ff74d429bc95dba40e5ebb7e5289b5b5c91375
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-09-03 05:27:02 +0530

    stripe checkout button
    stripe checkout button  is created
    button is integrated inside the checkout component
    new styles are added to the test card information text
    changing the favicon icon in the index.js

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,41 +1,49 @@
 import React from "react";
-import './checkout.styles.scss'
-import {connect} from "react-redux";
-import {createStructuredSelector} from "reselect";
-import {selectCartItems, selectCartItemTotal} from "../../redux/cart/cart.selectors";
+import "./checkout.styles.scss";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import {
+  selectCartItems,
+  selectCartItemTotal,
+} from "../../redux/cart/cart.selectors";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
-const CheckoutPage = ({cartItems, total}) => (
-    <div className={'checkout-page'}>
-        <div className={"checkout-header"}>
-            <div className={"header-block"}>
-                <span>Product</span>
-            </div>
-            <div className={"header-block"}>
-                <span>Description</span>
-            </div>
-            <div className={"header-block"}>
-                <span>Quantity</span>
-            </div>
-            <div className={"header-block"}>
-                <span>Price</span>
-            </div>
-            <div className={"header-block"}>
-                <span>Remove</span>
-            </div>
-        </div>
-        {
-            cartItems.map(
-                cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
-            )
-        }
-        <div className={"total"}>
-            <span>TOTAL ${total}</span>
-        </div>
+const CheckoutPage = ({ cartItems, total }) => (
+  <div className={"checkout-page"}>
+    <div className={"checkout-header"}>
+      <div className={"header-block"}>
+        <span>Product</span>
+      </div>
+      <div className={"header-block"}>
+        <span>Description</span>
+      </div>
+      <div className={"header-block"}>
+        <span>Quantity</span>
+      </div>
+      <div className={"header-block"}>
+        <span>Price</span>
+      </div>
+      <div className={"header-block"}>
+        <span>Remove</span>
+      </div>
     </div>
-)
+    {cartItems.map((cartItem) => (
+      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+    ))}
+    <div className={"total"}>
+      <span>TOTAL ${total}</span>
+    </div>
+    <div className={"test-warning"}>
+      * please use the following test credit card for payment *
+      <br />
+      4242 4242 4242 4242 EXP - 01/22 CVV - 123
+    </div>
+    <StripeCheckoutButton />
+  </div>
+);
 const mapStateToProps = createStructuredSelector({
-    cartItems: selectCartItems,
-    total: selectCartItemTotal
-})
+  cartItems: selectCartItems,
+  total: selectCartItemTotal,
+});
 export default connect(mapStateToProps)(CheckoutPage);
