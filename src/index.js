670915e7fd27189114421bb1ea9910d8af6bbb9b
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-09-02 06:08:28 +0530

    added the redux-persisit to the application

--- src/index.js
+++ src/index.js
@@ -3,20 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 // import reportWebVitals from "./reportWebVitals";
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from 'react-redux'
-import store from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
-    <Provider store={store}>
-        <React.StrictMode>
-            <BrowserRouter>
-                <App/>
-            </BrowserRouter>
-        </React.StrictMode>
-    </Provider>
-    ,
-    document.getElementById("root")
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // // If you want to start measuring performance in your app, pass a function
