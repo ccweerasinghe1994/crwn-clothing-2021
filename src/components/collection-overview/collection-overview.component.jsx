bde350de573837aa10d0cc726118b508b78f7019
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-09-02 20:03:32 +0530

    created a new selector for the collection preview

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { selectCollections } from "../../redux/shop/shop.selector";
+import { selectCollectionForPreview } from "../../redux/shop/shop.selector";
 import CollectionPreview from "../collection-preview/collection-preview.component";
 import "./collection-overview.styles.scss";
 
@@ -10,6 +10,6 @@ const CollectionOverview = ({ collections }) =>
     <CollectionPreview key={id} {...otherProps} />
   ));
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionForPreview,
 });
 export default connect(mapStateToProps)(CollectionOverview);
