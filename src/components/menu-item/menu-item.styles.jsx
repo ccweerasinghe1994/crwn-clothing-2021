a6d0d771dd4f4fd5b9c7a1f3a164471bf1822533
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-09-04 07:06:38 +0530

    menu item is converted

---
+++ src/components/menu-item/menu-item.styles.jsx
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+
+export const MenuItemContainer = styled.div`
+  min-width: 30%;
+  height: 240px;
+  flex: 1 1 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  margin: 0 7.5px 15px;
+  overflow: hidden;
+
+  &.large {
+    width: 380px;
+  }
+
+  &:first-child {
+    margin-right: 7.5px;
+  }
+
+  &:last-child {
+    margin-left: 7.5px;
+  }
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const BackGroundImageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-image: url(${(props) => props.imageUrl});
+  background-position: center;
+  background-size: cover;
+
+  ${MenuItemContainer}:hover & {
+    transform: scale(1.1);
+    transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+  }
+`;
+export const ContentContainer = styled.div`
+  height: 90px;
+  padding: 0 25px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  background: white;
+  opacity: 0.7;
+  position: absolute;
+  ${MenuItemContainer}:hover & {
+    opacity: 0.9;
+  }
+`;
+export const TitleContainer = styled.h1`
+  font-weight: bold;
+  margin-bottom: 6px;
+  font-size: 22px;
+  color: #4a4a4a;
+`;
+export const SubtitleContainer = styled.span`
+  font-weight: lighter;
+  font-size: 16px;
+`;
