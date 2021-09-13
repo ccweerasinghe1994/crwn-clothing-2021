f538f85fe1445e51287ca8843377183acf03b5a5
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-09-13 11:12:34 +0530

    converted the collection page

--- crwn-clothing graphQL/src/components/collections-overview/collection-overview.container.jsx
+++ crwn-clothing graphQL/src/components/collections-overview/collection-overview.container.jsx
@@ -22,9 +22,6 @@ const GET_COLLECTIONS = gql`
 const CollectionOverviewContainer = () => (
   <Query query={GET_COLLECTIONS}>
     {({ loading, error, data }) => {
-      console.log({ loading });
-      console.log({ error });
-      console.log({ data });
       if (loading) return <Spinner />;
       return <CollectionsOverview collections={data.collections} />;
     }}
