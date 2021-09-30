b8c5b7226cf836760931c91af433207e99558959
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-09-30 19:53:15 +0530

    Update App.js

--- react-basics/react-basics/src/App.js
+++ react-basics/react-basics/src/App.js
@@ -1,23 +1,47 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      monsters: [
+        { name: "frankenstein" },
+        { name: "dracula" },
+        { name: "zombie" },
+      ],
+    };
+  }
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello My name is chamara
-        </a>
-      </header>
-    </div>
-  );
+  handleClick = () => {
+    this.setState((state, props) => {
+      return {
+        string: "Hi Andrew",
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hi
+          </a>
+          <div></div>
+          <button onClick={this.handleClick}>change name</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
